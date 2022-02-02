f4f781a0d1ca66cfed7c55f020b0cf5f3d988159
Paulo H Santos <paulohenrique.uerj@gmail.com>
2022-02-02 18:00:29 -0300

    C# ex 008

---
+++ Ex008/Conversor de medidas/Program.cs
@@ -0,0 +1,9 @@
+﻿// Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+Console.Write("Digite um valor em metros: ");
+double metro = Convert.ToDouble(Console.ReadLine());
+double cent = Math.Round(metro * 100, 2);
+double mili = Math.Round(metro * 1000, 2);
+
+Console.WriteLine("{0}m em centímetros: {1}cm", metro, cent);
+Console.WriteLine("{0}m em milímetros: {1}mm", metro, mili);
